4ad8beb15fe1e9af8d04adde5f1ae964cabf1b5e
El cholo <jose.panta.mero@utelvt.edu.ec>
2022-09-22 19:09:33 -0500

    version1

---
+++ velocimetro.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+	float v,d,t;
+	cout<<"Ingrese la distancia(en km) d= "; cin>>d;
+	cout<<"Ingrese el tiempo(en horas)  t= "; cin>>t;
+	v=d/t;
+	cout<<"La velocidad calculada es de v= "<<t<<endl<<endl;
+	return 0;
+
+
+
+}
