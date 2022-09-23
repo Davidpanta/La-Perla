506bfe7dea3392b37365bc857763a56b673b4386
El cholo <jose.panta.mero@utelvt.edu.ec>
2022-09-22 19:37:17 -0500

    version2

--- contabiliza.cpp
+++ contabiliza.cpp
@@ -3,19 +3,19 @@ using namespace std;
 
 int main()
 {
-	int i=0,l;
-	float x,s=0;
-	cout<<"ingrese el limite l="; cin>>l;
+	int JDPM_i=0,JDPM_l;
+	float JDPM_x,JDPM_s=0;
+	cout<<"ingrese el limite JDPM_l="; cin>>JDPM_l;
 	do{
 		
-	cout<<"ingrese el numero x="; cin>>x;
-	i=i+1;
-	s=s+x;
+	cout<<"ingrese el numero JDPM_x="; cin>>JDPM_x;
+	JDPM_i=JDPM_i+1;
+	JDPM_s=JDPM_s+JDPM_x;
 
 
 
-	}while(i<l);
-	cout<<"Se ingresaron "<<l<<" numeros "<< "que sumaron "<<s<<endl;
+	}while(JDPM_i<JDPM_l);
+	cout<<"Se ingresaron "<<JDPM_l<<" numeros "<< "que sumaron "<<JDPM_s<<endl;
 	return 0;
 
 
