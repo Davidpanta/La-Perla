a79d8f8774d3012840cf218ec67e4bcd3753dd3c
El cholo <jose.panta.mero@utelvt.edu.ec>
2022-09-22 19:14:02 -0500

    version1

---
+++ compara.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	float a,b;
+	cout<<"ingrese a b:"; cin>>a>>b;
+	if(a>b)
+	{
+	  cout<<a<<" es mayor a "<<b<<endl;
+
+
+	}else{
+
+	  cout<<b<<" es mayor a "<<a<<endl;
+
+
+	}
+
+	return 0;
+
+
+
+
+
+
+}
