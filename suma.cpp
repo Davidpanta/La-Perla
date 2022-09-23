45fe1beb7b022103dab1681b15231b8dceecf1f7
El cholo <jose.panta.mero@utelvt.edu.ec>
2022-09-22 19:05:52 -0500

    version3

--- suma.cpp
+++ suma.cpp
@@ -1,4 +1,4 @@
-#include <iostream>
+#include <iostream> 
 using namespace std;
 int main ()
 {
@@ -6,7 +6,12 @@ int main ()
 	cout<<"Ingrese dos numeros A B: ";
 	cin>>A>>B;
 	C=A+B;
+	float x,y,z;
+	cout<<"Ingrese dos numeros separados de espacio x y:  ";
+	cin>>x>>y;
+	z=x+y;
 	cout<<"El resultado es: ";
 	cout<<C;
+	cout<<z<<endl;
 	return 0;
 }
